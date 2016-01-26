9d0094cd390609c43a53f0e42d08d8866df503c9
Przemysław Kuliga <przemyslaw.kuliga@schibsted.pl>
2016-01-26 09:44:54 +0100

    update

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+var PORT = process.env.PORT || 3005;
+var MONGOLAB_URI = process.env.MONGOLAB_URI || 'mongodb://heroku_rc6jxkln:jl60qp4df6khbm6tnh4j9139g6@ds035975.mongolab.com:35975/heroku_rc6jxkln';
+
 var app = require('./app')(require('./stock-repository')({
     //mongoURL: 'mongodb://localhost:27017/test'
-    mongoURL: 'mongodb://heroku_rc6jxkln:jl60qp4df6khbm6tnh4j9139g6@ds035975.mongolab.com:35975/heroku_rc6jxkln'
+    mongoURL: MONGOLAB_URI
 }));
 
-app.listen(3005, function () {
-    console.log('Express server is listening on port 3005');
+app.listen(PORT, function () {
+    console.log('Express server is listening on port ' + PORT);
 });
