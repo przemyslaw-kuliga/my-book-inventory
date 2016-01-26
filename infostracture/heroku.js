92df9536cec1704a36f38ca5aa2f4ebbcd6cfbca
Przemysław Kuliga <przemyslaw.kuliga@schibsted.pl>
2016-01-26 10:37:27 +0100

    update

--- infostracture/heroku.js
+++ infostracture/heroku.js
@@ -5,3 +5,24 @@ var configurator = heroin(process.env.HEROKU_API_TOKEN, {debug: false});
 configurator.export('my-book-inventory').then(function (result) {
     console.log(result);
 });
+
+configurator({ name: 'my-book-inventory',
+  region: 'eu',
+  maintenance: false,
+  stack: 'cedar-14',
+  config_vars: {},
+  addons: {},
+  collaborators: 
+   [ 'przemyslaw.kuliga@schibsted.pl',
+     'lukasz.adamczuk@schibsted.pl',
+     'woj.niemiec@gmail.com' ],
+  features: 
+   { 'runtime-dyno-metadata': { enabled: false },
+     'log-runtime-metrics': { enabled: false },
+     'http-session-affinity': { enabled: false },
+     preboot: { enabled: false },
+     'http-shard-header': { enabled: false },
+     'http-end-to-end-continue': { enabled: false } },
+  formation: [ { process: 'web', quantity: 1, size: 'Free' } ],
+  log_drains: [],
+  domains: [ 'my-book-inventory.herokuapp.com' ] });
