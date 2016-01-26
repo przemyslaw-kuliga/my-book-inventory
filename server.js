283ab7e9e1223c16ba8fd81d4a605348cfa56abe
Przemysław Kuliga <przemyslaw.kuliga@schibsted.pl>
2016-01-26 09:37:14 +0100

    update

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var app = require('./app')(require('./stock-repository')({
-    mongoURL: 'mongodb://localhost:27017/test'
+    //mongoURL: 'mongodb://localhost:27017/test'
+    mongoURL: 'mongodb://heroku_rc6jxkln:jl60qp4df6khbm6tnh4j9139g6@ds035975.mongolab.com:35975/heroku_rc6jxkln'
 }));
 
 app.listen(3005, function () {
